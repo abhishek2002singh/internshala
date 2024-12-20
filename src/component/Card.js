c6deb43aaa13c7e3d79f3873424b7cf252f17d36
abhishek singh <singhsabhishek23@gmail.com>
2024-12-20 08:58:16 +0530

    make responsive

--- src/component/Card.js
+++ src/component/Card.js
@@ -2,21 +2,20 @@ import React from 'react';
 
 const Card = ({ user }) => {
   return (
-    <div className="max-w-xl w-full bg-white border-2 border-gray-300 shadow-lg rounded-lg p-6 flex items-center gap-6 transform transition duration-300 hover:scale-105 hover:shadow-2xl hover:border-blue-500 cursor-pointer">
-     
-      <div className="flex-shrink-0">
+    <div className="max-w-xl w-full bg-white border-2 border-gray-300 shadow-lg rounded-lg p-6 flex flex-col sm:flex-row items-center gap-6 transform transition duration-300 hover:scale-105 hover:shadow-2xl hover:border-blue-500 cursor-pointer">
+      <div className="flex-shrink-0 mb-4 sm:mb-0 sm:w-32 sm:h-32">
         <img
           src={user.picture.large}
           alt="User"
-          className="w-32 h-32 rounded-md border border-gray-400"
+          className="w-32 h-32 sm:w-32 sm:h-32 rounded-md border border-gray-400 object-cover"
         />
       </div>
 
-      
       <div className="flex-grow">
         <div className="flex items-center justify-between">
-          <h2 className="text-lg font-semibold text-gray-800">{user.name.first +" "+user.name.last}</h2>
-         
+          <h2 className="text-lg font-semibold text-gray-800">
+            {user.name.first + ' ' + user.name.last}
+          </h2>
         </div>
         <p className="text-gray-600 mt-2">
           <strong>Gender:</strong> {user.gender}
